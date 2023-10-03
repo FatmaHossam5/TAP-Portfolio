5849cf9aaad4197ee83110675de0e533929e43a5
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2023-10-04 01:00:48 +0300

    answer the remain assigmaint in Js

---
+++ main .js
@@ -0,0 +1,16 @@
+function missingNums(arr){
+    let missedNum=0;
+    arr.sort((a,b)=>a-b)
+    for(let i=0;i<arr.length;i++){
+        let sub=arr[i+1]-arr[i]
+        if(sub>1){
+            for(let j=1;j<sub;j++){
+                missedNum=arr[i]+j
+            }
+        }
+    }
+    console.log(missedNum)
+    return missedNum
+}
+missingNums([9,6,4,2,3,5,7,0,1])
+
